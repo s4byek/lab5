31902b1612384e4a17b39d22d3cfa1fd2423e67f
s4byek <youexample.com>
2025-12-07 20:07:09 +0300

    changes in Main.java in dev

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.print("Это добавлено в ветке dev");
+        System.out.println("Это аналогичные изменения но с другим содержанием");
+    }
+}
