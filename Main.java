7ad28f7a42f9009df272e9167ad9891af8a105e2
s4byek <youexample.com>
2025-12-07 23:07:17 +0300

    third in dev: print3 method

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 class Main{
     public static void main(String[] args) {
         printMessage();
+        print3();
     }
     public static void printMessage() {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Это аналогичные изменения но с другим содержанием");
         System.out.println("Это значимые изменения для первого комита");
     }
+    public static void print3(){
+        System.out.println("Это значительные изменения для третьего коммита");
+    }
+
 }
