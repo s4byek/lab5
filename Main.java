302e4efb3dbfeba508e02b5f8ad41b14464e016a
s4byek <youexample.com>
2025-12-07 22:50:16 +0300

    second in dev: prtinMessage method

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main{
     public static void main(String[] args) {
+        printMessage();
+    }
+    public static void printMessage() {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Это аналогичные изменения но с другим содержанием");
         System.out.println("Это значимые изменения для первого комита");
