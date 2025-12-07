8e041696059a9cd9c124f0464c4eced526f6a37a
s4byek <youexample.com>
2025-12-07 22:45:29 +0300

    first in dev

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Это аналогичные изменения но с другим содержанием");
+        System.out.println("Это значимые изменения для первого комита");
     }
 }
